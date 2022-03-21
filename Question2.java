1fa73e2af4122cb42e269b393d319558f83fdd97
aarti-lather <97521221+aarti-lather@users.noreply.github.com>
2022-03-21 11:15:08 +0530

    question2

---
+++ Question2.java
@@ -0,0 +1,54 @@
+package evaluation3;
+
+public class Question2 {
+
+}
+
+
+
+
+
+
+
+
+// **************************************question 3:-*************************************************
+//    What is the difference between final and finally in Java?
+
+//    finall:-
+//            1.) final  keyword is use with the classes,method and variables.
+//            2.) final is the keyword and access modifier, use to restrictions on class,method & vriable.
+//            3.) final method cannot overridden by sub class.
+//            4.) cannot  be inherited.
+//
+//        finally:-
+//            1.) finally block is related to try catch blok.
+//            2.) finally block is xecuted as soon as the try catch block is excuted.
+//        3.) its run the important code even after exceptiong handleing.
+
+
+// **************************************question 2:-*************************************************
+//     throw:-
+//             throw keyword, used an exception expilicty  in the code, block of code.
+//            1.) throw is used in method.
+//             2) throw kwyword is folled by an instace of exception to be thrown.
+//            3) allow only one exception at a time.
+//
+//throws:-
+//
+//             throws keyword is used in the method to declare an exception ,thrown by the funciton  while the excution the code
+//             1) throws is used in method signature.
+//             2)throws kwyword is folled by class names of exception to be thrown.
+//             3) we can declare multipl exception using throws kryword.
+//eg.-----> SQL Excepion
+// **************************************question 1:-*************************************************
+//checked:
+//      1)  CLass not found
+//      2)  IO Exception
+//      3)  SQL exception
+//      4)  Fle Not Found exception
+//
+//uncheck:
+//        1)     Airthmatic exception
+//        2)   nullPointer exception
+//        3)    Array index out of bound
+//        4)  illegalthread
